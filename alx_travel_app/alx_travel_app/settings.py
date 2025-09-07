98ceb6fd5c8ae0ab7e77ed672763e99598a8f951
kimoban <isaiahkimoban87@gmail.com>
2025-09-07 15:04:33 +0000

    fix: update ALLOWED_HOSTS for Render deployment

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-restore-this-with-your-own-secret-key'
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
